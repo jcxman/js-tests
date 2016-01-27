dd844349dbce1cd07bd98a0f7ff442d8a997bc61
chenxinji <695881071@qq.com>
2016-01-26 20:09:24 -0500

    commit current tests to current folder

---
+++ app/logicalOperators.js
@@ -0,0 +1,24 @@
+//exports = (typeof window === 'undefined') ? global : window;
+//
+//exports.logicalOperatorsAnswers = {
+//  or : function(a, b) {
+//
+//  },
+//
+//  and : function(a, b) {
+//
+//  }
+//};
+//
+//
+exports = (typeof window === 'undefined') ? global : window;
+
+exports.logicalOperatorsAnswers = {
+  or : function(a, b) {
+    return a || b;
+  },
+
+  and : function(a, b) {
+    return a && b;
+  }
+};
